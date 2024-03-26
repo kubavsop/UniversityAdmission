f694901460e357d5c34d2e08b55a5e25a79408a6
maksim <mazhur04@gmail.com>
2024-03-26 16:40:44 +0700

    Update user db context

---
+++ Admission.User.Domain/Entities/StudentAdmission.cs
@@ -0,0 +1,15 @@
+﻿using Admission.Domain.Common.Entities;
+using Admission.Domain.Common.Enums;
+
+namespace Admission.User.Domain.Entities;
+
+public class StudentAdmission: BaseEntity
+{
+    public Manager? Manager { get; set; }
+    public Guid? ManagerId { get; set; }
+    public AdmissionStatus Status { get; set; }
+    public Guid? FirstPriorityFacultyId { get; set; }
+    public Faculty? FirstPriorityFaculty;
+    public Guid ApplicantId { get; set; }
+    public Applicant? Applicant { get; set; }
+}
