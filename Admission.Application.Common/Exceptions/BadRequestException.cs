cc731663278bc00d88666776dd2225c435cecab7
maksim <mazhur04@gmail.com>
2024-03-29 21:14:32 +0700

    Add basic exceptions

---
+++ Admission.Application.Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace Admission.Application.Common.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException() {}
+    
+    public BadRequestException(string message) : base(message) {}
+}
---
+++ Admission.Application.Common/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace Admission.Application.Common.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException() {}
+
+    public ForbiddenException(string message) : base(message) {}
+
+    public ForbiddenException(Guid userId) : base($"Access is forbidden for user with id=({userId})") {}
+}
